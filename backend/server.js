59405b5c7d3221e5597e403cd11f8aac03aa5645
Ati0199 <art019900@mail.ru>
2025-09-20 22:29:50 +0400

    created endpoint for support (forms) and created inital express app

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import sqlite3 from "sqlite3";
+import cors from "cors";
+import multer from "multer";
+
+const app = express();
+const PORT = 4200;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/support", (req, res) =>{
+    // const {name, phone} = req.body
+
+    return res.status(200).send({"message": "success"});
+})
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
